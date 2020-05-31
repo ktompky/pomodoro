263729e30c74d5d70262321a4590352da9a28d4c
Kelly <k.tompkins005@gmail.com>
2020-05-31 06:35:01 -0500

    adding tkinter for gui goodness

--- pomodoro.py
+++ pomodoro.py
@@ -1,15 +1,29 @@
 import time
+import datetime as dt
 
-#display the programs instructions
+import tkinter
+from tkinter import messagebox
+import winsound
 
-print("Press enter to begin. Then press enter again to click the stopwatch. Press ctrl-c to quit.")
+#current time
+t_now  = dt.datetime.now()
+#pomodoro time
+t_pom = 25*60
+#time delta in mins
+t_delta = dt.timedelta(0,t_pom)
+#ending pomodoro
+t_fut = t_now + t_delta
+#break time
+delta_sec = 5*60
+#ya final time.
+t_fin = t_now + dt.timedelta(0,t_pom+t_delta)
 
 
-input() #this is when you would press enter to begin
 
-print('Started')
 
-startTime = time.time() #get first laps start time
-lastTime = startTime
-lapNum = 1
-#need to track lap times
+
+
+
+
+
+ 
