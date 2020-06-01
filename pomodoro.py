e506ce032faf79cb8c90644ab8968c3fe7a5de16
Kelly <k.tompkins005@gmail.com>
2020-06-01 09:39:45 -0500

    adding messagebox

--- pomodoro.py
+++ pomodoro.py
@@ -16,7 +16,7 @@ t_fut = t_now + t_delta
 #break time
 delta_sec = 5*60
 #ya final time.
-t_fin = t_now + dt.timedelta(0,t_pom+t_delta)
+t_fin = t_now + dt.timedelta(0,t_pom+delta_sec)
 
 #hides the main window for tkinter, only shows alert message box
 root = tkinter.Tk()
@@ -40,6 +40,31 @@ while True:
                 winsound.Beep((i+100), 700)
             print('Break time!')
             breaks+= 1
+    else:
+        print('finished')
+        breaks =0
+
+    #letting user know that the break is over
+        for i in range(10):
+            winsound.Beep((i+100), 500)
+
+            usr_ans = messagebox.askyesno("Pomodoro finished. Would you like to start one again?")
+            total_pomodoros += 1
+            if usr_ans == True:
+                t_now = dt.datetime.now()
+                t_fut = t_now + dt.timedelta(0,t_pom)
+                t_fin = t_now + dt.timedelta(0,t_pom+delta_sec)
+                continue
+            elif usr_ans == False:
+                messagebox.showinfo("Pomodoro finished.", "\nYou completed " + str(total_pomodoros)+" pomodoros today!")
+                break
+
+    print('sleeping')
+    time.sleep(20)
+    t_now = dt.datetime.now()
+    timenow = t_now.strftime("%H:%M")
+
+
 
 
 
