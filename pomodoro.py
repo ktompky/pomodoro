00d4e16b17fc91618f0733c84246032a9b48d968
Kelly <k.tompkins005@gmail.com>
2020-06-20 16:24:26 -0500

    adding breaks

--- pomodoro.py
+++ pomodoro.py
@@ -1,23 +1,8 @@
 import time
 import datetime
 
-# def workTime():
-#     on_minutes = 25 * 60
-#     ##Let's use a ranged loop to create the counter
-#     for i in range(on_minutes):
-#         print(str(datetime.timedelta(on_minutes-i)) + " seconds remaining \n")
-#     ##we also need the loop to wait for 1 second between each iteration
-#         t.sleep(1)
 
-#     print("Time is up")
-
-
-# workTime()
-
-
-
-
-def countdown(t):
+def workCountdown(t):
     t = t * 60 
     while t:
         mins, secs = divmod(t, 60)
@@ -25,6 +10,40 @@ def countdown(t):
         print(timeformat, end='\r')
         time.sleep(1)
         t -= 1
-    print('Goodbye!\n\n\n\n\n')
+    print('Time for a break!\n\n\n\n\n')
 
-countdown(25)
+def shortBreak(t):
+    t = t * 60
+    while t:
+        mins, secs = divmod(t, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        t -= 1
+    print('Time for a break!\n\n\n\n\n')
+
+def longBreak(t):
+    t = t * 60
+    while t:
+        mins, secs = divmod(t, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        t -= 1
+    print('Time for a break!\n\n\n\n\n')
+
+def gameBreak(t):
+    t = t * 60
+    while t:
+        mins, secs = divmod(t, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        t -= 1
+    print('Time for a break!\n\n\n\n\n')
+
+
+workCountdown(25)
+shortBreak(5)
+longBreak(15)
+gameBreak(60)
