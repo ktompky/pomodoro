938a9e719bf06325bcd9e1e890a7435d3841996a
Kelly <k.tompkins005@gmail.com>
2022-01-08 19:04:45 -0600

    creating the notifications for pomodoro

--- pomodoro.py
+++ pomodoro.py
@@ -1,61 +1,14 @@
 import time
-import datetime
+from plyer import notification
+
+count = 0
+print("The pomodoro timer has started, start working.")
 
 
-def workCountdown(t):
-    t = t * 60 
-    while t:
-        mins, secs = divmod(t, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeformat, end='\r')
-        time.sleep(1)
-        t -= 1
-    print('Time for a break!\n')
 
-def shortBreak(t):
-    t = t * 60
-    while t:
-        mins, secs = divmod(t, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeformat, end='\r')
-        time.sleep(1)
-        t -= 1
-    print('Time to go back to work!\n')
-
-# def longBreak(t):
-#     t = t * 60
-#     while t:
-#         mins, secs = divmod(t, 60)
-#         timeformat = '{:02d}:{:02d}'.format(mins, secs)
-#         print(timeformat, end='\r')
-#         time.sleep(1)
-#         t -= 1
-#     print('Time for a break!\n\n\n\n\n')
-
-# def gameBreak(t):
-#     t = t * 60
-#     while t:
-#         mins, secs = divmod(t, 60)
-#         timeformat = '{:02d}:{:02d}'.format(mins, secs)
-#         print(timeformat, end='\r')
-#         time.sleep(1)
-#         t -= 1
-#     print('Time for a break!\n\n\n\n\n')
-start_cycle = 1 
-print("Welcome to the kitty cat pomodoro!\n\nHow long do you want to work? You will pick the number of cycles you want.")
-print("A cycle is 30 minutes: A 25 block of work time and a 5 minute block of break time.")
-
-end_cycle = int(input("How many cycles do you want? "))
-
-
-def cycle(start_cycle, end_cycle):
-    workCountdown(25)
-    shortBreak(5)
-    if start_cycle < end_cycle:
-        start_cycle += 1
-        cycle(start_cycle,end_cycle)
-    else:
-        print(start_cycle)
-        print("Your work is finished!")
-
-cycle(start_cycle,end_cycle)
+if __name__ == "__main__"
+    while True:
+        notification.notify(
+            title = "Good Work, minion.",
+            message = "Please actually take a 10 min break. You've completed " + str(c)
+        )
